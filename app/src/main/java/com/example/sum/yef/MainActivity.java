a5ce006355965a741a727642cd6c508367383b01
some1-unknown <sharshil1299@gmail.com>
2018-06-13 21:08:24 +0530

    Fully Running App

--- app/src/main/java/com/example/sum/yef/MainActivity.java
+++ app/src/main/java/com/example/sum/yef/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionbar.setHomeAsUpIndicator(R.mipmap.loho_round);
 
 
         NavigationView navigationView = findViewById(R.id.nav_view);
