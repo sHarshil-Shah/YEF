17ed11101876b50c530c6f1309b5c83360bd5359
some1-unknown <sharshil1299@gmail.com>
2018-06-15 23:26:52 +0530

    About Us page improvements

--- app/src/main/java/com/example/sum/yef/Main3Activity.java
+++ app/src/main/java/com/example/sum/yef/Main3Activity.java
@@ -1,7 +1,11 @@
 package com.example.sum.yef;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        Button btnFb = findViewById(R.id.btn_fb);
+        Button btnInsta = findViewById(R.id.btn_insta);
+        Button btnYoutube = findViewById(R.id.btn_youtube);
+
+        btnFb.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                String url = "https://www.facebook.com/yefindia.in";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        btnInsta.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                String url = "www.youtube.com/channel/UCLs5bATz7-gV6CagwgW7wVw";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
     }
 }
